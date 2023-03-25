d1d9df74ec1bdd5d2c0d629b429379dbd01fdd8f
anno59 <372025284@qq.com>
2023-03-25 15:10:29 +0800

    feat: add async action

--- src/containers/AddTodoContainer.js
+++ src/containers/AddTodoContainer.js
@@ -11,4 +11,23 @@ const mapDispatchToProps = (dispatch) => ({
   addTodo: (text) => dispatch(addToDo(text)),
 });
 
+function fetchData(message, dispatch) {
+  dispatch({ type: "LOADING", message });
+  setTimeout(() => {
+    dispatch({ type: "LOADED" });
+  }, 1000);
+}
+
+function App() {
+  const onClick = () => {
+    fetchData("data is loading", dispatch);
+  };
+
+  return (
+    <div>
+      <button onClick={onClick}>click</button>
+    </div>
+  );
+}
+
 export default connect(mapStateToProps, mapDispatchToProps)(AddTodo);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./component/App";
 import { createStore } from "redux";
 import todoApp from "./reducer";
 import { Provider } from "react-redux";
+import { List, Map } from "immutable";
 
 const store = createStore(todoApp);
 
